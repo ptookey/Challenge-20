205bf3baaf713facec1040f679bb8b0a5635c7e2
tookey95 <petetookey@gmail.com>
2023-09-17 12:01:27 -0400

    restarted from scratch

--- src/main.jsx
+++ src/main.jsx
@@ -1,73 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './Header.jsx'
+import App from './App.jsx'
 import './index.css'
-import Home from './Home.jsx'
-import Footer from './Footer.jsx'
-import Profile from './Profile.jsx'
-import Trip from './Trip.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route
-        path="/"
-        element={<Home/>}>
-        </Route>
-        <Route
-        path="/profile"
-        element={<Profile/>}
-        >
-        </Route>
-        <Route
-        path="/profile/trip"
-        element={<Trip/>}
-        >
-        </Route>
-      </Routes>
-      <Footer/>
-    </Router>
+    <App />
   </React.StrictMode>,
 )
-
-
-{/* <ApolloProvider client={client}>
-<Router>
-  <div className="flex-column justify-flex-start min-100-vh">
-    <Header />
-    <div className="container">
-      <Routes>
-        <Route 
-          path="/" 
-          element={<Home />} 
-        />
-        <Route 
-          path="/login" 
-          element={<Login />} 
-        />
-        <Route 
-          path="/signup" 
-          element={<Signup />} 
-        />
-        <Route 
-          path="/me" 
-          element={<Profile />} 
-        />
-        <Route 
-          path="/profiles/:profileId" 
-          element={<Profile />} 
-        />
-         <Route 
-          path="/create" 
-          element={<Create />} 
-        />
-      </Routes>
-    </div>
-    <Footer />
-  </div>
-</Router>
-</ApolloProvider> */}
